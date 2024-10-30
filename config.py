3a84e610474cf205437db9a59bf6c0110375415e
alexdpg23 <alexpatino@unicauca.edu.co>
2024-10-30 15:29:23 -0500

    primer comentario

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://entrega1_cw71_user:0oPb6gD6vjTTntnb6lSXomt2YExctzoD@dpg-csh4fc3tq21c73e3bvlg-a.oregon-postgres.render.com/entrega1_cw71'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
